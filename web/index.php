9761db400abb304d6eb7246ac203fc2a0f6e9eef
Nick Hamze <me@nickhamze.com>
2019-10-06 11:22:30 -0500

    Updated Design
    Updated Design

--- web/index.php
+++ web/index.php
@@ -9,101 +9,176 @@ foreach ($fontfiles as $fontfile) {
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ASCII by sorta stupid</title>
+    <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    
     <style type="text/css">
         body {
-            padding-top: 30px;
+          background-color: black;
+          background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);
+          height: 100vh;
+          margin: 0;
+          overflow: hidden;
+          padding: 0;
+          color: white;
+          font: 1.3rem monospace;
+          text-shadow: 0 0 5px #C8C8C8;
+        }
+        
+        body::after {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          background: repeating-linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15) 1px, transparent 1px, transparent 2px);
+          pointer-events: none;
         }
 
-        pre {
-            display: inline-block;
-            font-family: monospace;
-            line-height: initial;
-            padding: 10px;
+        ::selection {
+          background: #0080FF;
+          text-shadow: none;
         }
-
-        .modal-dialog{
-            position: relative;
-            display: table; /* This is important */
-            overflow-y: auto;
-            overflow-x: auto;
-            width: auto;
-            min-width: 300px;
+        
+        #top {
+            color: #000;
+            background-color: #00ff00;
+            width: 100%;
+            padding: 2px;
+            margin-bottom: 20px;
         }
-
+        
+        #text {
+            position: absolute;
+            top: -9999px;
+            left: -9999px;
+        }
+        
+        #outputFigDisplay {
+            
+        }
+        
+            pre {
+                display: inline-block;
+                font-family: monospace;
+                line-height: initial;
+                padding: 10px;
+                color: #00ff00;
+                margin: 0;
+                font-size: 1.5vw;
+            }
+            
+            @media (min-width: 1024px) {
+				pre {
+					font-size: 1vw;
+				}
+			}
+            
+        .blink {
+            animation: blink-animation 1s steps(5, start) infinite;
+            -webkit-animation: blink-animation 1s steps(5, start) infinite;
+            width: 5px;
+            background-color: #00ff00;
+            height: 100%;
+        }
+            
+            @keyframes blink-animation {
+              to {
+                visibility: hidden;
+              }
+            }
+            @-webkit-keyframes blink-animation {
+              to {
+                visibility: hidden;
+              }
+            }
+        
+        #bottom {
+            position: fixed;
+            width: 100%;
+            bottom: 0%;
+        }
+        
+            #bottom ul {
+                list-style: none;
+                padding: 0;
+                margin: 0;
+            }
+            
+                #bottom li {
+                    cursor: pointer;
+                }
+                
+                    #bottom li span {
+                        display: inline-block;
+                        color: #000;
+                        background-color: #00ff00;
+                        width: 30px;
+                        text-align: center;
+                    }
+    
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome and Opera */
+    }
+    
+    .modal-dialog {
+        position: relative;
+        display: table; /* This is important */ 
+        overflow-y: auto;    
+        overflow-x: auto;
+        width: auto;
+        min-width: 300px;   
+    }
+    
+    
+    .flex {
+      display: flex;
+      justify-content: space-between;
+      flex-flow: row wrap;
+    }
+    
+    .inside-right {
+      flex: 1;
+    }
+        
     </style>
 </head>
 <body>
-    <main role="main" class="container">
-        <div class="row mb-4">
-            <div class="col-sm-12">
-                <div class="card">
-                  <div class="card-header">boring text</div>
-                  <div class="card-body">
-                      <div class="form-group">
-                        <textarea class="form-control" id="inputText" placeholder="Write something here" required>sorta stupid</textarea>
-                      </div>
-                  </div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="row mb-4">
-            <div class="col-sm-12">
-                <div class="card">
-                  <div class="card-header">
-                        <div class="form-inline">
-                            <span class="mr-sm-2">fun text</span>
-
-                            <button id="btn-copy" type="button" class="mr-sm-2 btn btn-primary btn-sm" data-clipboard-target="#outputFigDisplay > pre">Copy</button>
-                            <button id="btn-save" type="button" class="mr-sm-2 btn btn-primary btn-sm">Save</button>
-
-                            <select class="mr-sm-2 form-control" id="font">
-                                <?php for ($i = 0; $i < count($fonts); $i++): ?>
-                                    <option value="<?php echo $i ?>"><?php echo htmlentities($fonts[$i]) ?></option>
-                                <?php endfor; ?>
-                            </select>
-
-                            <button class="mr-sm-2 btn btn-sm btn-primary" id="btn-left" title="Previous font">←</button>
-                            <button class="mr-sm-2 btn btn-sm btn-primary" id="btn-right" title="Next font">→</button>
-                        </div>
-                  </div>
-                  <div class="card-body">
-
-                    <div id="outputFigDisplay"></div>
-
-                  </div>
-                </div>
-            </div>
-        </div>
-    </main>
-
-    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Save image</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-              <div class="image"></div>
-              <p><em>(right click, save image)</em></p>
-          </div>
-        </div>
-      </div>
+    <div id="top">
+        <span>sorta stupid 1.0</span>
     </div>
-
+    
+    <textarea id="text"></textarea>
+    
+    <div class="flex">
+      <div class="inside inside-left"><div id="outputFigDisplay"></div></div>
+      <div class="inside inside-right"><div class="blink"></div></div>
+    </div>
+    
+    <div id="bottom">
+        <ul>
+            <li id="btn-left"><span>←</span> Previous Font</li>
+            <li id="btn-right"><span>→</span> Next Font</li>
+            <li id="btn-copy"><span>^c</span> Copy Text</li>
+            <li id="btn-save"><span>^s</span> Convert to Image</li>
+        </ul>
+    </div>
+    
 <!--
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/3.0.0/fetch.min.js" integrity="sha256-E1M+0f/hvoNVoV8K5RSn1gwe4EFwlvORnOrFzghX0wM=" crossorigin="anonymous"></script>
 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    
+    <script type="text/javascript" src="js/jquery.detect_swipe.js"></script>
     <script type="text/javascript" src="js/figlet.js"></script>
     <script type="text/javascript" src="js/clipboard.min.js"></script>
     <script type="text/javascript" src="js/html2canvas.min.js"></script>
@@ -111,20 +186,23 @@ foreach ($fontfiles as $fontfile) {
 <script>
     (function($) {
         var fonts = <?php echo json_encode($fonts) ?>;
-
+        
         if (window.location.protocol === 'file:') {
             alert('fetch APi does not support file: protocol.');
         }
-
+        
         figlet.defaults({
             fontPath: 'fonts'
         });
-
-        var counter = parseInt($('#font').val());
-
+        
+        var text = localStorage.getItem('text') || "sorta stupid";
+        var counter = parseInt(localStorage.getItem('counter') || 18);
+        
         var update = function() {
             var fontName = fonts[counter];
-            var inputText = $('#inputText').val();
+            var inputText = text;
+            
+            localStorage.setItem('text', text);
 
             figlet(inputText, {
                 font: fontName,
@@ -152,70 +230,124 @@ foreach ($fontfiles as $fontfile) {
             });
             */
         };
-
-        $('#inputText').on('keyup', function(e) {
-            update();
-        });
-
-        $(document).on('keyup', function(e) {
-            if (e.keyCode == 37 /* LEFT */) {
-                counter = (counter - 1 < 0) ? fonts.length : counter - 1;
-                $('#font').val(counter);
-                update();
-            }
-
-            else if (e.keyCode == 39 /* RIGHT */) {
-                counter = counter + 1 > fonts.length ? counter = 0 : counter + 1;
-                $('#font').val(counter);
-                update();
-            }
-        });
-
-        $('#btn-left').on('click', function() {
+        
+        var previousFont = function() {
             counter = (counter - 1 < 0) ? fonts.length : counter - 1;
-            $('#font').val(counter);
+            localStorage.setItem('counter', counter);
+            //~ $('#font').val(counter);
             update();
-        });
-
-        $('#btn-right').on('click', function() {
+        }
+        
+        var nextFont = function() {
             counter = counter + 1 > fonts.length ? counter = 0 : counter + 1;
-            $('#font').val(counter);
+            localStorage.setItem('counter', counter);
+            //~ $('#font').val(counter);
             update();
+        }
+        
+        $('#text').val(text);
+        
+        $('#text').on('blur', function() {
+            $('#text').focus();
         });
-
-        $('#font').on('change', function() {
-            counter = parseInt($(this).val());
-            update();
+        
+        $(document).on('keydown click', function() {
+            $('#text').focus();
         });
-
-        update(); // init
-
-        var clipboard = new ClipboardJS('#btn-copy');
-
+        
+        // Disable arrow keys in (hidden) textarea
+        $('#text').bind('keydown', function(e) {
+            var key = event.keyCode || event.charCode;
+            
+            if (key === 37 /* LEFT */ || key === 39 /* RIGHT */) {
+                e.preventDefault();
+            }
+        });
+        
+        // Monitor textarea
+        $('#text').bind('keyup', function(e) {
+            var key = event.keyCode || event.charCode;
+            
+            if (key === 37 /* LEFT */) {
+                previousFont();
+            }
+            
+            else if (key === 39 /* RIGHT */) {
+                nextFont();
+            }
+            
+            else {
+                text = $(this).val();
+                update();
+            }
+        });
+        
+        $('#btn-left').on('click', function() {
+            previousFont();
+        });
+        
+        $('#btn-right').on('click', function() {
+            nextFont();
+        });
+        
+        $(document).on('swipeleft', function() {
+			previousFont();
+		});
+		
+		$(document).on('swiperight', function() {
+			nextFont();
+		});
+        
+        var clipboard = new ClipboardJS('#btn-copy', {
+            text: function(trigger) {
+                update();
+                console.log(trigger);
+                return $("#outputFigDisplay > pre").text();
+            }
+        });
+        
         $('#btn-copy').tooltip({
             title: 'Copied!',
             placement: 'bottom',
             trigger: 'manual',
         });
-
+        
         clipboard.on('success', function(e) {
             $('#btn-copy').tooltip('show');
             e.clearSelection();
             setTimeout(function() {
                 $('#btn-copy').tooltip('hide');
-            }, 1000);
+            }, 5000);
         });
-
-
+        
+         $('#btn-save').tooltip({
+            title: 'Converted!<br>(right click to save as image)',
+            placement: 'bottom',
+            trigger: 'manual',
+            html: true
+        });
+        
         $('#btn-save').on('click', function() {
-            html2canvas(document.querySelector("#outputFigDisplay > pre")).then(canvas => {
-                $('#modal .modal-body .image').html('');
-                $('#modal .modal-body .image').append(canvas);
-                $('#modal').modal('show');
+            update();
+            html2canvas(document.querySelector("#outputFigDisplay > pre"), {
+                backgroundColor: 'transparent'
+            }).then(canvas => {
+                $('#btn-save').tooltip('show');
+                
+                $('#outputFigDisplay').html('');
+                $('#outputFigDisplay').append('<img src="' + canvas.toDataURL() + '">');
+                
+                setTimeout(function() {
+                    $('#btn-save').tooltip('hide');
+                }, 5000);
             });
         });
+        
+        // Init
+        
+        update();
     })(jQuery);
 </script>
-
+    
 </body>
 </html>
